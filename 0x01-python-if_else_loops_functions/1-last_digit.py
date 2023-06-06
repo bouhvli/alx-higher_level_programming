470ab9d742bdd173e35eae8f7bd433d2d032336a
bouhvli <bouhali.hamza.2@gmail.com>
2023-06-06 13:07:47 +0100

    change the string format

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,18 +7,20 @@ def get_last(number):
     if number == 0:
         return 0
     number = number % 10
+    if number < 0:
+        number = number * (-1)
     return number
 
 
 if get_last(number) > 5:
     print(
-        f"Last digit of {number: d} is{get_last(number): d} "
-        "and is greater than 5"
+        "Last digit of {} is {} "
+        "and is greater than 5".format(number, get_last(number))
         )
 elif get_last(number) < 6 and get_last(number) > 0:
     print(
-        f"Last digit of {number: d} is{get_last(number): d} "
-        "and is less than 6 and not 0"
+        "Last digit of {} is {} "
+        "and is less than 6 and not 0".format(number, get_last(number))
         )
 else:
-    print(f"Last digit of {number: d} is{get_last(number): d} and is 0")
+    print("Last digit of {} is {} and is 0".format(number, get_last(number)))
