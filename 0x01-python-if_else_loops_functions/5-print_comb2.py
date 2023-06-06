c0b97dfe05e505a16d1fd0f3a824c4b5d198e487
bouhvli <bouhali.hamza.2@gmail.com>
2023-06-06 13:27:40 +0100

    changing string format

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    print(f"{i:02d}", end='')
+    print("{:02d}".format(i), end='')
     if i == 99:
         break
-    print(f", ", end='')
+    print(", ", end='')
