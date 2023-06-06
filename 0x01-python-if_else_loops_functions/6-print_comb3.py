27476ce153f2d3d96846e06e372b5f75a7b94f5b
bouhvli <bouhali.hamza.2@gmail.com>
2023-06-06 13:36:36 +0100

    change the logic

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
-    for x in range(i+1, 10):
-        print(f"{i :d}{x :d}", end='')
+    for x in range(i + 1, 10):
         if (i == 8 and x == 9):
-            break
-        print(f", ", end='')
+            print("{}{}".format(i, x))
+        else:
+            print("{}{}".format(i, x), end=', ')
