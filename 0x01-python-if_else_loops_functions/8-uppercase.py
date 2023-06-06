5bde20437682de3a59bb98e19ae47dc4f1a8ab08
bouhvli <bouhali.hamza.2@gmail.com>
2023-06-06 13:50:49 +0100

    change the last number of the ascii code

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     sil = list(str)
     for i in range(0, len(str)):
         lower = ord(str[i])
-        if lower in range(97, 122):
+        if lower in range(97, 123):
             sil[i] = chr(lower - 32)
         else:
             continue
