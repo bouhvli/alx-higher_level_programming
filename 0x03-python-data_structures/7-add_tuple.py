7a868f5ed584215d2fcec02405b3df6289e334d1
bouhvli <bouhali.hamza.2@gmail.com>
2023-06-10 16:18:41 +0100

    update the conditions

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    va1, va2 = tuple_a + (0,) * (2 - len(tuple_a))
-    vb1, vb2 = tuple_b + (0,) * (2 - len(tuple_b))
+    va1 = tuple_a[0] if len(tuple_a) >= 1 else 0
+    va2 = tuple_a[1] if len(tuple_a) >= 2 else 0
+    vb1 = tuple_b[0] if len(tuple_b) >= 1 else 0
+    vb2 = tuple_b[1] if len(tuple_b) >= 2 else 0
     som1 = va1 + vb1
     som2 = va2 + vb2
-    new_tuple = (som1, som2)
-    return new_tuple
+    return tuple((som1, som2))
