95c490b587221a3767825375cd5c34c05636aad5
bouhvli <bouhali.hamza.2@gmail.com>
2023-06-10 16:21:47 +0100

    update code

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,11 +2,8 @@
 def no_c(my_string):
     my_list = list(my_string)
     up_list = []
-    if (my_list.count('c') != 0 or my_list.count('C') != 0):
-        for i in my_list:
-            if i.upper() != 'C':
-                up_list.append(i)
-            else:
-                continue
-        new_string = ''.join(up_list)
-        return (new_string)
+    for i in my_list:
+        if i.upper() != 'C':
+            up_list.append(i)
+    new_string = ''.join(up_list)
+    return (new_string)
