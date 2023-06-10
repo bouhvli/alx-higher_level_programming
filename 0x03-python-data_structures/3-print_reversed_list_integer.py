a5e574c5d8a10b15eccda6de57f0d02368990372
bouhvli <bouhali.hamza.2@gmail.com>
2023-06-10 15:59:20 +0100

    pushing the new sol

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,6 @@
 def print_reversed_list_integer(my_list=[]):
     if len(my_list) == 0:
         return (None)
-    for i in range(1, len(my_list) + 1):
-        print("{:d}".format(my_list[-i]))
+    new_list = my_list[::-1]
+    for i in range(len(new_list)):
+        print("{:d}".format(new_list[i]))
