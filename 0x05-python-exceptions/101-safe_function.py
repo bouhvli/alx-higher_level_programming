8f969b483711d9b3897425277c17113bedd277fa
bouhvli <bouhali.hamza.2@gmail.com>
2023-06-26 13:56:19 +0100

    pushing task 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    var1, var2 = args
+    try:
+        res = fct(var1, var2)
+    except ValueError as joy:
+        print("Exception: {}".format(joy), file=sys.stderr)
+        return (None)
+    except TypeError as joy:
+        print("Exception: {}".format(joy), file=sys.stderr)
+        return (None)
+    except IndexError as joy:
+        print("Exception: {}".format(joy), file=sys.stderr)
+        return (None)
+    except ZeroDivisionError as joy:
+        print("Exception: {}".format(joy), file=sys.stderr)
+        return (None)
+    return (res)
