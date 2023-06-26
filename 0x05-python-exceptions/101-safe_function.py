4b2aee23e45ad8567746619d7d0fcc3b3b3f7bc4
bouhvli <bouhali.hamza.2@gmail.com>
2023-06-26 13:58:50 +0100

    cleaning the sol

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,19 +3,9 @@ import sys
 
 
 def safe_function(fct, *args):
-    var1, var2 = args
     try:
-        res = fct(var1, var2)
-    except ValueError as joy:
-        print("Exception: {}".format(joy), file=sys.stderr)
-        return (None)
-    except TypeError as joy:
-        print("Exception: {}".format(joy), file=sys.stderr)
-        return (None)
-    except IndexError as joy:
-        print("Exception: {}".format(joy), file=sys.stderr)
-        return (None)
-    except ZeroDivisionError as joy:
+        res = fct(*args)
+    except Exception as joy:
         print("Exception: {}".format(joy), file=sys.stderr)
         return (None)
     return (res)
