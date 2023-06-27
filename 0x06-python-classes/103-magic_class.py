bcf23fafdeb189fd646c68ac29ca4f2c83d2788e
bouhvli <bouhali.hamza.2@gmail.com>
2023-06-27 15:50:24 +0100

    add comments

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
+"""10. ByteCode -> Python #5
+Write the Python class MagicClass that does exactly
+the same as the following Python bytecode
+"""
 import math
 
 
 class MagicClass:
+    """Class called MagicClass represent the magical
+    cercle with radius.
+    Attributes:
+        __radius (float): the radius.
+    """
     def __init__(self, radius):
         self.__radius = 0
         if type(radius) != int and type(radius) != float:
@@ -10,7 +19,15 @@ class MagicClass:
         self.__radius = radius
 
     def area(self):
+        """get the area of a circle
+        Returns:
+            number (float): the result
+        """
         return math.pi * (self.__radius ** 2)
 
     def circumference(self):
+        """get the circumference of the circle
+        Returns:
+            number (float): the result
+        """
         return 2 * math.pi * self.__radius
