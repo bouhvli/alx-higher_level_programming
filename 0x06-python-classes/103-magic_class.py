071e855233925208e08dde02f8fb46197a92d427
bouhvli <bouhali.hamza.2@gmail.com>
2023-06-27 15:58:40 +0100

    update initial attr

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -12,7 +12,7 @@ class MagicClass:
     Attributes:
         __radius (float): the radius.
     """
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
@@ -23,7 +23,7 @@ class MagicClass:
         Returns:
             number (float): the result
         """
-        return math.pi * (self.__radius ** 2)
+        return (self.__radius ** 2) * math.pi
 
     def circumference(self):
         """get the circumference of the circle
