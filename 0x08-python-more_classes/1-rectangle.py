d4668023e05f3fbed4c0a7364a9e105f073bc17e
bouhvli <bouhali.hamza.2@gmail.com>
2023-07-03 16:22:10 +0100

    change the setter logic

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,48 +15,24 @@ class Rectangle:
 
     @property
     def height(self):
-        """The getter for the __height
-
-        Returns: (int) the height value.
-        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Setter function for height.
-        Args:
-            value (int): The value that __height will hold
-                    if value is == int and > 0
-        Attributes:
-            __height (int): Private instance attribute
-        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
 
     @property
     def width(self):
-        """The getter for the __width
-
-        Returns: (int) the width value.
-        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Setter function for height.
-        Args:
-            value (int): The value that __height will hold
-                    if value is == int and > 0
-        Attributes:
-            __height (int): Private instance attribute
-        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
