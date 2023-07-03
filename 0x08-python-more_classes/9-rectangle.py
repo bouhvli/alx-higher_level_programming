aa96904c153905eb015369694dff1230001e54ef
bouhvli <bouhali.hamza.2@gmail.com>
2023-07-03 16:58:13 +0100

    push

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -14,8 +14,8 @@ class Rectangle:
     print_symbol = '#'
 
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
         Rectangle.number_of_instances += 1
 
     @property
@@ -37,6 +37,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
+        print(value)
         if type(value) != int:
             raise TypeError("width must be an integer")
         elif value < 0:
