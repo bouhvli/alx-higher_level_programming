bef9c1abb5c3f4c22a6d30e9438e34d437d73ce4
bouhvli <bouhali.hamza.2@gmail.com>
2023-07-03 16:30:15 +0100

    push

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle:
         __height (int): the height input value
     """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def height(self):
