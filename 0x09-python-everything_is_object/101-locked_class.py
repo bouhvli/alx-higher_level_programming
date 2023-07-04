cb1099b25246f77d9c104526f17f5e51317f279d
bouhvli <bouhali.hamza.2@gmail.com>
2023-07-04 13:39:12 +0100

    push the task 30

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,17 +3,4 @@
 
 
 class LockedClass:
-    """This class LockedClass with no class or object attribute
-    prevents the user from dynamically creating new instance
-    attributes, except if the new instance attribute is called
-    first_name.
-    """
-    def __init__(self):
-        self.first_name = None
-
-    def __setattr__(self, __name: str, __value):
-        if __name == "first_name" or hasattr(self, __name):
-            object.__setattr__(self, __name, __value)
-        else:
-            raise AttributeError("'LockedClass' object has "
-                                 "no attribute '{}'".format(__name))
+    __slots__ = ['first_name']
