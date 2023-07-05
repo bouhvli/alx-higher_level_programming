640ce97df741ed199722a23ae32555ab2c335bf4
bouhvli <bouhali.hamza.2@gmail.com>
2023-07-05 14:11:03 +0100

    check case

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -27,3 +27,5 @@ class TestMaxInteger(unittest.TestCase):
         my_list = [random.randint(1, 999) for _ in range(99)]
         my_list.append(1000)
         self.assertEqual(max_integer(my_list), 1000)
+        my_list = [-1, -2, -3, 189, -5, -6]
+        self.assertEqual(max_integer(my_list), 189)
