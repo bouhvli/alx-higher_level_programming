99dc1389088f0686273d02e4ce652010e9288191
bouhvli <bouhali.hamza.2@gmail.com>
2023-07-05 14:17:01 +0100

    check the #

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""
+Multiply matrix using the numpy module
+"""
 import numpy
 
 
 def lazy_matrix_mul(m_a, m_b):
+    """ lazy mul using num py """
     try:
         new_mat = numpy.dot(m_a, m_b)
         return new_mat
