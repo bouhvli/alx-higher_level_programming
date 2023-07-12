f66238b0398f0df67dcb89910955667dac5a46f1
bouhvli <bouhali.hamza.2@gmail.com>
2023-07-12 15:14:32 +0100

    push

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,14 +2,16 @@
 """
 Task 0: read file and print it
 """
+
+import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-import sys
 
-file  = "add_item.json"
+
+file = "add_item.json"
 try:
     data = load_from_json_file(file)
-except:
+except Exception:
     data = None
 
 if data is None:
