60122ab84286b4ad270159ea7652c959f8593f42
bouhvli <bouhali.hamza.2@gmail.com>
2023-07-12 15:12:34 +0100

    adding try

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-"""Module
-Task 7: Load, add, save a script that adds all arguments
-to a Python list, and then save
-them to a file
 """
-
-
-import sys
-
-
+Task 0: read file and print it
+"""
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+import sys
 
-file = "add_item.json"
-data = load_from_json_file(file)
+file  = "add_item.json"
+try:
+    data = load_from_json_file(file)
+except:
+    data = None
 
 if data is None:
     data = []
-
 data.extend(sys.argv[1:])
 save_to_json_file(data, file)
