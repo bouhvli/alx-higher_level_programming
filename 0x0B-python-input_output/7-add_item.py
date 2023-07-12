4025a9557cefcb29ebe22c3fd7c955cda2568cfa
bouhvli <bouhali.hamza.2@gmail.com>
2023-07-12 14:17:38 +0100

    delete the extra #

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """
-Task 7: Load, add, save
+Task 7: Load, add, save a script that adds all arguments
+to a Python list, and then save
+them to a file
 """
 
 
 import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-"""a script that adds all arguments to a Python list, and then save
-them to a file"""
+
+
 file = "add_item.json"
 data = load_from_json_file(file)
 if data is None:
