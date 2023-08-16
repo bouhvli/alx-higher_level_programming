74c3d20cd19bf8a3d7d0432a6d313cffcdd93ad9
bouhvli <bouhali.hamza.2@gmail.com>
2023-08-16 17:52:11 +0100

    adding the name of the col

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- lists all Comedy shows in the database hbtn_0d_tvshows.
-SELECT tv_shows.title
+SELECT tv_shows.title AS 'title'
 FROM tv_shows JOIN tv_show_genres JOIN tv_genres
 ON tv_genres.name = 'Comedy'
 WHERE tv_genres.id = tv_show_genres.genre_id
