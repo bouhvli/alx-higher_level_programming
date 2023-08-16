ac3c84d8f79a1104a74bb02a6aeee0c79a30ca8d
bouhvli <bouhali.hamza.2@gmail.com>
2023-08-16 18:24:08 +0100

    add task 2 from advanced tasks

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,11 +1,8 @@
--- 
-SELECT DISTINCT tv_shows.title AS 'title'
-FROM tv_shows INNER JOIN tv_show_genres 
-ON tv_show_genres.show_id = tv_shows.id
-INNER JOIN tv_genres
-ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_shows.title NOT IN (
-    SELECT tv_shows.title
+-- lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
+SELECT tv_shows.title AS 'title'
+FROM tv_shows
+WHERE tv_shows.id NOT IN (
+    SELECT tv_shows.id
     FROM tv_shows INNER JOIN tv_show_genres 
     ON tv_show_genres.show_id = tv_shows.id 
     INNER JOIN tv_genres
