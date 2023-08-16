13c0aea8b529c8cad3d7ba6433dcc1926f7f8783
bouhvli <bouhali.hamza.2@gmail.com>
2023-08-16 17:45:34 +0100

    update the script even if it returns the same result

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,8 @@
 -- lists all genres of the show Dexter.
+
 SELECT tv_genres.name
-FROM tv_genres JOIN tv_show_genres JOIN tv_shows
-ON tv_shows.title = 'Dexter'
-WHERE tv_shows.id = tv_show_genres.show_id
-AND tv_genres.id = tv_show_genres.genre_id;
+FROM tv_genres JOIN tv_show_genres 
+ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_shows
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_shows.title = 'Dexter';
