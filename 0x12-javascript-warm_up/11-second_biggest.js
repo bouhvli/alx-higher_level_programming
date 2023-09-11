3d41403d0ea1686fd376ac655e2619812157187e
bouhvli <bouhali.hamza.2@gmail.com>
2023-09-11 19:26:50 +0100

    update the logic

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) { console.log(0); } else {
+if (process.argv.length <= 3) { console.log('0'); } else {
   const array = [];
   process.argv.flatMap(function (n) {
     const vn = parseInt(n);
