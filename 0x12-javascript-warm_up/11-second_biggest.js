9820d7c159b0f740f5e6a9941f71f1ba63806372
bouhvli <bouhali.hamza.2@gmail.com>
2023-09-11 19:22:46 +0100

    update the logic

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) { console.log(0); } else {
   const array = process.argv.sort();
-  console.log(array[array.length - 2]);
+  console.log(array.reverse()[1]);
 }
