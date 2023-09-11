ef97f7181738868677010ca5c0ee962a90112445
bouhvli <bouhali.hamza.2@gmail.com>
2023-09-11 18:52:04 +0100

    update the ES module

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-import { argv } from 'node:process';
-if (argv.length <= 2) {
+if (process.argv.length <= 2) {
   console.log('No argument');
 } else {
-  argv.forEach((val, idx) => {
+  process.argv.forEach((val, idx) => {
     if (idx >= 2) {
       console.log(`${val}`);
     }
