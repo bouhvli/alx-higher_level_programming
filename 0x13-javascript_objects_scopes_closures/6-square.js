33617bfafbe979c3ad2bee33a247fcabef428e4d
bouhvli <bouhali.hamza.2@gmail.com>
2023-09-12 14:39:26 +0100

    update the logic

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,11 +4,7 @@ const NewSquare = require('./5-square');
 class Square extends NewSquare {
   charPrint (c) {
     for (let index = 0; index < this.height; index++) {
-      let line = '';
-      for (let jdx = 0; jdx < this.width; jdx++) {
-        if (c === undefined) { line += 'X'; } else { line += 'C'; }
-      }
-      console.log(line);
+      if (c === undefined) { console.log('X'.repeat(this.width)); } else { console.log('C'.repeat(this.width)); }
     }
   }
 }
