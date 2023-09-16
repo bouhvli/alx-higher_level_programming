8837765c530b01bad8559a52ac3b5e579d4e346d
bouhvli <bouhali.hamza.2@gmail.com>
2023-09-16 13:22:40 +0100

    update the SQL

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -34,7 +34,7 @@ def result():
     db = establising_connection()
     cur = db.cursor()
     cur.execute(
-        "SELECT id, name FROM states WHERE name LIKE 'N%'  ORDER BY id ASC")
+        "SELECT * FROM states WHERE name LIKE 'N%'  ORDER BY id ASC")
     res = cur.fetchall()
     return res
 
