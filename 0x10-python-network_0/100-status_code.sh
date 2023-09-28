2fb7fdaf1956d3a00c313107928307897f830a7a
bouhvli <bouhali.hamza.2@gmail.com>
2023-09-28 07:42:53 -0700

    update sol witout pipes

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a request to a URL passed as an argument, and displays only the status code of the response.
-curl -s --head $1 | awk '/^HTTP/{print $2}' | tr -d '\n'
+curl -s --output /dev/null --write-out "%{http_code}" $1
