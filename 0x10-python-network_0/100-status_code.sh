ee83900b12a0873613f3d7195ebdf145929762e4
bouhvli <bouhali.hamza.2@gmail.com>
2023-09-28 07:39:26 -0700

    deleting the new line

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a request to a URL passed as an argument, and displays only the status code of the response.
-curl -s --head $1 | awk '/^HTTP/{print $2}'
+curl -s --head $1 | awk '/^HTTP/{print $2}' | tr -d '\n'
