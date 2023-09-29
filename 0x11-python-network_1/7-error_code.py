00408d21319157c6ba16da3284326a76bef490cc
bouhvli <bouhali.hamza.2@gmail.com>
2023-09-29 12:25:43 +0100

    adding task 7 updated

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-""""""
+"""
+script that takes in a URL, sends a request
+to the URL and displays the body of the response.
+"""
 from sys import argv
 import requests
 from requests.exceptions import HTTPError
 
 
 if __name__ == '__main__':
-    try:
-        res = requests(argv[1])
-        print(res.__dict__)
-    except HTTPError as er:
-        print('Error code: {}'.format(er.code))
+    res = requests.get(argv[1])
+    if (res.status_code < 400):
+        print(res._content.decode('utf-8'))
+    else:
+        print('Error code: {}'.format(res.status_code))
