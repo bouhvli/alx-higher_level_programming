b8396bc5d8e4922378c12a4c61843f92827c7a5a
bouhvli <bouhali.hamza.2@gmail.com>
2023-09-29 10:30:12 +0100

    update the output

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     with urlopen('https://alx-intranet.hbtn.io/status') as res:
         response = res.read()
         print('Body response:')
-        print('    - type: {}'.format(type(response)))
-        print('    - content: {}'.format(response))
-        print('    - utf8 content: {}'.format(response.decode('utf-8')))
+        print('\t- type: {}'.format(type(response)))
+        print('\t- content: {}'.format(response))
+        print('\t- utf8 content: {}'.format(response.decode('utf-8')))
