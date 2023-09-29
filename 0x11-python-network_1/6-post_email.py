ce4fd63836964732c0f5c69b4bf224122111948e
bouhvli <bouhali.hamza.2@gmail.com>
2023-09-29 12:12:15 +0100

    adding task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+    script that takes in a URL and an email address,
+    sends a POST request to the passed URL with the email
+    as a parameter, and finally displays the body of the response
+"""
 from sys import argv
 import requests
 
@@ -7,4 +12,4 @@ if __name__ == '__main__':
     url = argv[1]
     email = {'email': argv[2]}
     res = requests.post(url, data=email)
-    print(res.decode('utf-8'))
+    print(res._content.decode('utf-8'))
