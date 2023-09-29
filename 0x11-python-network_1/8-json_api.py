052995aabefb8dd5010055529d7d49324cc4b3e6
bouhvli <bouhali.hamza.2@gmail.com>
2023-09-29 16:08:02 +0100

    adding task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,7 +10,7 @@ from sys import argv
 
 if __name__ == '__main__':
     url = "http://0.0.0.0:5000/search_user"
-    if argv[1]:
+    if len(argv) == 2:
         data = {'q': argv[1]}
     else:
         data = {'q': ""}
