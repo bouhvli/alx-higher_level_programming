b11cff49d4af5057ff3250c5ef7029c9ced5ae13
bouhvli <bouhali.hamza.2@gmail.com>
2023-09-29 11:19:52 +0100

    adding task 3 and updating the output

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,6 +8,6 @@ from urllib.error import HTTPError
 if __name__ == '__main__':
     try:
         with urlopen(argv[1]) as res:
-            print(res)
+            print(res.read().decode('utf-8'))
     except HTTPError as er:
-        print(er.code)
+        print('Error code:'.format(er.code))
