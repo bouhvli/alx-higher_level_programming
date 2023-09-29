ebda4ee08a2559bf29bb59a9bfe6c4cca3a3fdef
bouhvli <bouhali.hamza.2@gmail.com>
2023-09-29 17:52:50 +0100

    adding task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+script that takes your GitHub credentials (username
+and password) and uses the GitHub API to display your id.
+"""
+from requests import get
+from sys import argv
+
+
+if __name__ == '__main__':
+    repo = argv[2]
+    owner = argv[1]
+
+    git_res = get('https://api.github.com/repos/{}/{}/commits'
+                  .format(owner, repo))
+    json = git_res.json()
+
+    try:
+        for i in range(10):
+            sha = json[i].get('sha')
+            name = json[i].get('commit').get('author').get('name')
+            print("{}: {}".format(sha, name))
+    except IndexError:
+        pass
